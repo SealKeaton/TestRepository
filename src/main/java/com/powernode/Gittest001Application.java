f448ec892d39d01c3e2b702dba0d1c6e3abd2a2a
seal <740124224@qq.com>
2024-10-29 08:59:23 +0800

    初次提交

---
+++ src/main/java/com/powernode/Gittest001Application.java
@@ -0,0 +1,13 @@
+package com.powernode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gittest001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gittest001Application.class, args);
+    }
+
+}
